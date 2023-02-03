d7a6dc9b0cd7fe8633e994d1d272a21bde02babe
Sarper MUTLUBAY <sarper.mutlubay@marti.tech>
2023-02-03 17:03:39 +0300

    sample file url changed.

--- TradeArt.CaseStudy.Model/Requests/CaseStudy/CalculateShaRequest.cs
+++ TradeArt.CaseStudy.Model/Requests/CaseStudy/CalculateShaRequest.cs
@@ -3,6 +3,6 @@ using System.ComponentModel;
 namespace TradeArt.CaseStudy.Model.Requests.CaseStudy;
 
 public class CalculateShaRequest {
-	[DefaultValue("https://speed.hetzner.de/100MB.bin")]
+	[DefaultValue("https://file-examples.com/wp-content/uploads/2017/02/zip_10MB.zip")]
 	public string FileUrl { get; set; }
 }
